49713f57ff29b2c3557180626a2eafc0691e5cc5
Edward Gao <egao@sent.com>
2017-03-21 01:30:02 -1000

    load test cards

--- src/containers/board/board.js
+++ src/containers/board/board.js
@@ -3,6 +3,12 @@ import './board.css';
 import Card from '../../components/Card';
 
 class Board extends Component {
+	onEdit = (event) => {
+		// edit button
+	}
+	onDel = (event) => {
+		// delete button
+	}
 	render() {
 		return (
 			<div className="Board">
@@ -15,13 +21,55 @@ class Board extends Component {
 						priority="Medium"
 						by="Jon"
 						to="Renee"
+						onEdit={this.onEdit}
+						onDel={this.onDel}
+					/>
+					<Card 
+						id="1"
+						task="Make Better Styles. Make Better Styles. Make Better Styles. Make Better Styles. Make Better Styles. Make Better Styles. "
+						type="queue-card"
+						priority="Medium"
+						by="Jon"
+						to="Renee"
+						onEdit={this.onEdit}
+						onDel={this.onDel}
 					/>
 				</div>
 				<div className="progress">
 					<div className="board-title">IN PROGRESS</div>
+					<Card 
+						id="1"
+						task="Make Better Styles."
+						type="progress-card"
+						priority="Medium"
+						by="Jon"
+						to="Renee"
+						onEdit={this.onEdit}
+						onDel={this.onDel}
+					/>
 				</div>
 				<div className="done">
 					<div className="board-title">DONE</div>
+					<Card 
+						id="1"
+						task="Make Better Styles."
+						type="done-card"
+						priority="Medium"
+						by="Jon"
+						to="Renee"
+						onEdit={this.onEdit}
+						onDel={this.onDel}
+					/>
+					<Card 
+						id="1"
+						task="Make Better Styles. Make Better Styles. Make Better Styles. Make Better Styles. Make Better Styles. Make Better Styles. "
+						type="done-card"
+						priority="Medium"
+						by="Jon"
+						to="Renee"
+						onEdit={this.onEdit}
+						onDel={this.onDel}
+					/>
 				</div>
 			</div>
 		);
