6ec306ad7e756f47ed899fea78ef5d88ca1ec80f
Edward Gao <egao@sent.com>
2017-03-20 23:37:10 -1000

    restructure project

---
+++ src/containers/app/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+				<h1>Kanban</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/app/index';
 import './index.css';
 
 ReactDOM.render(
