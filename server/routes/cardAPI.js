435a454968ed88ce719fb22693817eff7c3f60bb
Edward Gao <egao@sent.com>
2017-03-21 15:31:14 -1000

    PUT and DELETE cards

--- server/routes/cardAPI.js
+++ server/routes/cardAPI.js
@@ -9,12 +9,30 @@ const { Card } = db;
 
 router.get('/all', (req, res) => {
 	Card.findAll()
-		.then(cards => res.send(cards));
+		.then(cards => res.send(cards))
+		.catch(_ => res.send({"success": false}));
 });
 
 router.post('/new', ({body: {title, priority, by, to}}, res) => {
 	Card.create({type: "queue-card", title, priority, by, to})
-		.then(card => res.send(card));
+		.then(card => res.send(card))
+		.catch(_ => res.send({"success": false}));
+});
+
+router.put('/edit/:id', (req, res) => {
+	const {title, type, priority, by, to} = req.body;
+	Card.update(
+		{title, type, priority, by, to},
+		{where: {id: req.params.id}}
+	)
+		.then(card => res.send(card))
+		.catch(_ => res.send({"success": false}));
+});
+
+router.delete('/delete/:id', (req, res) => {
+	Card.destroy({where: {id: req.params.id}})
+		.then(_ => res.send({"success": true}))
+		.catch(_ => res.send({"success": false}));
 });
 
 module.exports = router;
