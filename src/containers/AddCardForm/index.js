53981a2cd953e286f0dd62c32ef0202c58fac805
kwokbk <kwokbk@hawaii.edu>
2017-08-15 19:46:05 -1000

    AddCardForm component

---
+++ src/containers/AddCardForm/index.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addCard } from '../../actions/cardActions';
+
+class AddCardForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+      priority: 'low',
+      status: 'in-queue',
+      createdBy: '',
+      assignedTo: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFieldChange = this.handleFieldChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addCard(this.state);
+    this.setState({
+      title: '',
+      priority: 'low',
+      status: 'in-queue',
+      createdBy: '',
+      assignedTo: ''
+    });
+  }
+
+  handleFieldChange(e) {
+    const newState = {};
+    const field = e.target.dataset.field;
+    newState[field] = e.target.value;
+
+    this.setState(newState);
+  }
+
+  render() {
+    const { title, priority, status, createdBy, assignedTo } = this.state;
+    return (
+      <form>
+        <input
+          type="text"
+          data-field="title"
+          onChange={this.handleFieldChange}
+          value={title}
+          placeholder="title"
+        />
+        <input
+          type="text"
+          data-field="priority"
+          onChange={this.handleFieldChange}
+          value={priority}
+          placeholder="priority"
+        />
+        <input
+          type="text"
+          data-field="status"
+          onChange={this.handleFieldChange}
+          value={status}
+          placeholder="status"
+        />
+        <input
+          type="text"
+          data-field="createdBy"
+          onChange={this.handleFieldChange}
+          value={createdBy}
+          placeholder="createdBy"
+        />
+        <input
+          type="text"
+          data-field="assignedTo"
+          onChange={this.handleFieldChange}
+          value={assignedTo}
+          placeholder="assignedTo"
+        />
+        <button type="submit" onClick={this.handleSubmit}>
+          submit
+        </button>
+      </form>
+    );
+  }
+}
+
+export const mapDispatchToProps = dispatch => {
+  return {
+    addCard: bindActionCreators(addCard, dispatch)
+  };
+};
+
+export default connect(null, mapDispatchToProps)(AddCardForm);
