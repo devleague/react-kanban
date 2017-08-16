00613d9948195b81b7cb18566d206cce767337c0
kwokbk <kwokbk@hawaii.edu>
2017-08-15 17:45:40 -1000

    app and column setup

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Column from '../Column';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <Column />
+        <Column />
+        <Column />
       </div>
     );
   }
---
+++ src/containers/Column/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Column.css";
+
+class Column extends Component {
+  render() {
+    return <div className="column" />;
+  }
+}
+
+export default Column;
