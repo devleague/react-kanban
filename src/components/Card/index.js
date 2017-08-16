d0d0dfd06cda1f23cc31ddac6d399cfae25a9038
kwokbk <kwokbk@hawaii.edu>
2017-08-15 23:54:11 -1000

    drag functionality for move_card action

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,14 +5,53 @@ class Card extends Component {
   constructor(props) {
     super(props);
 
-    this.state = this.props;
+    this.state = {
+      ...this.props,
+      isDragging: false
+    };
+
+    this._onDragStart = this._onDragStart.bind(this);
+    this._onDrag = this._onDrag.bind(this);
+    this._onDragEnd = this._onDragEnd.bind(this);
+  }
+
+  _onDragStart(e) {
+    console.log(this.state._id)
+    console.log(this.cardNode)
+    e.dataTransfer.setData('text', this.state._id);
+    e.dataTransfer.setData('application/x-moz-node type', this.cardNode);
+  }
+
+  _onDrag(e) {
+    this.setState({ isDragging: true });
+  }
+
+  _onDragEnd(e) {
+    this.setState({ isDragging: false});
   }
 
   render() {
-    const { _id, title, priority, status, createdBy, assignedTo } = this.props;
+    const {
+      _id,
+      title,
+      priority,
+      status,
+      createdBy,
+      assignedTo,
+      isDragging
+    } = this.state;
+
+    const className = isDragging ? 'card hidden' : 'card';
 
     return (
-      <div className="card" draggable={true}>
+      <div
+        className={className}
+        ref={node => this.cardNode = node}
+        draggable={true}
+        onDragStart={this._onDragStart}
+        onDrag={this._onDrag}
+        onDragEnd={this._onDragEnd}
+      >
         <h3>
           {title}
         </h3>
--- src/containers/Column/index.js
+++ src/containers/Column/index.js
@@ -3,11 +3,26 @@ import "./Column.css";
 import Card from '../../components/Card/';
 
 class Column extends Component {
+  constructor() {
+    super();
+
+    this._onDragOver = this._onDragOver.bind(this);
+    this._onDrop = this._onDrop.bind(this);
+  }
+
+  _onDragOver(e) {
+    e.preventDefault();
+  }
+
+  _onDrop(e) {
+    let data = e.dataTransfer.getData('text');
+    console.log(data);
+  }
   render() {
     const { type, cards } = this.props;
 
     return (
-      <div id={type} className="column">
+      <div id={type} onDrop={this._onDrop} onDragOver={this._onDragOver} className="column">
         {cards.map(card => <Card key={card._id} {...card} />)}
       </div>
     )
