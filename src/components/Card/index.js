54509496004adec42861e1bd9cce726ec2690383
kwokbk <kwokbk@hawaii.edu>
2017-08-15 22:31:44 -1000

    Card Component is now smart for upcoming crud ops

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Card.css';
 
-const Card = ({ _id, title, priority, status, createdBy, assignedTo }) =>
-  {
-    return <div className='card'>
-    <h3>{title}</h3>
-    <h5>Priority: {priority}</h5>
-    <h5>Created by: {createdBy}</h5>
-    <h5>Assigned to: {assignedTo}</h5>
-  </div>;
+class Card extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = this.props;
+  }
+
+  render() {
+    const { _id, title, priority, status, createdBy, assignedTo } = this.props;
+
+    return (
+      <div className="card" draggable={true}>
+        <h3>
+          {title}
+        </h3>
+        <h5>
+          Priority: {priority}
+        </h5>
+        <h5>
+          Created by: {createdBy}
+        </h5>
+        <h5>
+          Assigned to: {assignedTo}
+        </h5>
+      </div>
+    );
+  }
 }
 
 export default Card;
