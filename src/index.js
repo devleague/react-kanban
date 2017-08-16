898fff5c1b89c3f7ce1cd242ca5cbf60004a0cb3
kwokbk <kwokbk@hawaii.edu>
2017-08-15 15:15:56 -1000

    dlt non-used files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+render(<App />, document.getElementById('root'));
