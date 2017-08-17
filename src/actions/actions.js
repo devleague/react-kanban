5310e4dc402dd2dbbc548e4c56082beef15db1a2
kwokbk <kwokbk@hawaii.edu>
2017-08-16 14:45:50 -1000

    remove actions not being used

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,3 +3,7 @@ export const ADD_CARD = 'ADD_CARD';
 export const DEL_CARD = 'DEL_CARD';
 
 export const MOVE_CARD = 'MOVE_CARD';
+
+export const REQUEST_CARDS = 'REQUEST_CARDS';
+
+export const RECEIVE_CARDS = 'RECEIVE_CARDS';
