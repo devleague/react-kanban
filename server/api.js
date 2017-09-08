0bc9fb7aba2d9650aabdc82a9796eb38f7dfe507
Benjamin Kwok <benkwokdev@gmail.com>
2017-09-08 13:56:46 -1000

    moveCard status merged with fakeDb update api, rename data to fakeDb

--- server/api.js
+++ server/api.js
@@ -1,25 +1,60 @@
 const router = require('express').Router();
-const api = require('./data.js');
+const api = require('./fakeDb.js');
 
-router.route('/cards')
-.get((req, res) => {
-  api.getFakeDbReq()
-  .then(cards => {
-    res.json(cards);
-  })
-})
-.post((req, res) => {
-  let { card } = req.body;
-  api.addToFakeDb(card)
-  .then(newCard => {
-    console.log("----------------------")
-    console.log(newCard)
+router
+  .route('/cards')
+  .get((req, res) => {
     api.getFakeDbReq().then(cards => {
-      console.log("ALTERED DB")
-      console.log(cards);
-    })
-    res.json(newCard);
+      res.json({
+        status: 'success',
+        data: cards
+      });
+    });
   })
-});
+  .post((req, res) => {
+    let { card } = req.body;
+    api.addToFakeDb(card).then(newCard => {
+      api.getFakeDbReq().then(cards => {});
+      res.json({
+        status: 'success',
+        data: newCard
+      });
+    });
+  });
+
+router
+  .route('/cards/:id')
+  .delete((req, res) => {
+    api
+      .delFromFakeDb(req.params.id)
+      .then(card => {
+        res.json({
+          status: 'success',
+          data: null
+        });
+      })
+      .catch(err => {
+        res.json({
+          status: 'fail',
+          message: err
+        });
+      });
+  })
+  .patch((req, res) => {
+    api
+      .updateFromFakeDb(req.params.id, req.body)
+      .then(card => {
+        res.json({
+          status: 'success',
+          data: card
+        });
+      })
+      .catch(err => {
+        res.json({
+          status: 'fail',
+          message: err
+        });
+      });
+  });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
 app.use(bodyParser.json());
