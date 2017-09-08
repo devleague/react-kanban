e5c35378eb2daf6826856793c14a25dcb1e61c1d
Benjamin Kwok <benkwokdev@gmail.com>
2017-09-07 18:35:28 -1000

    added react dev tools

--- src/createStore.js
+++ src/createStore.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 export default function() {
-  const store = createStore(reducers, applyMiddleware(logger, thunk));
+  const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
 
   return store;
 }
