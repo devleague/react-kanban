04c67e85b95134fb72ce1b0aa4f53d8e47648ce4
Benjamin Kwok <benkwokdev@gmail.com>
2017-09-12 00:05:41 -1000

    card fields

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Field = ({label, value}) =>
-  <div>
-    {label}: {value}
-  </div>
+const Field = ({label, value, className}) =>
+    <div className>
+      {label && label + ': '} {value}
+    </div>
 
 export default Field;
