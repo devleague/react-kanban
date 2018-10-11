9225381a20bf5793c8f4ccfdfbedade4e3f5528c
Abby Zhang <abbyzhang@Abbys-MBP.home>
2018-10-10 20:45:02 -1000

    📖  DOC: start react

---
+++ server/db/knex.js
@@ -0,0 +1,6 @@
+const environment = process.env.ENVIRONMENT || 'development';
+const config = require('../knexfile.js')[environment];
+module.exports = require('knex')({ debug: true, ...config })
+
+
+
---
+++ server/knexfile.js
@@ -0,0 +1,57 @@
+// Update with your config settings.
+require('dotenv').config({ path: '../.env' })
+
+console.log('process.env', process.env)
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.POSTGRES_HOSTNAME,
+      database: process.env.POSTGRES_DB,
+      port: process.env.POSTGRES_CONTAINER_PORT,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.POSTGRES_HOSTNAME,
+      database: process.env.POSTGRES_DB,
+      port: process.env.POSTGRES_CONTAINER_PORT,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  }
+};
