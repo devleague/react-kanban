9d07e713c7ab90509eceb1257af252a1e933b34a
Sarah <yamashige.sarah@gmail.com>
2018-10-14 18:41:03 -1000

    separating the components

--- src/queue/Queue.js
+++ src/queue/Queue.js
@@ -1,21 +1,46 @@
-import React, { Component } from 'react';
-
+// import React, { Component } from 'react';
+import React from 'react';
 import '../App.css';
 
-class Queue extends Component {
-    constructor(props) {
-        super(props)
-    }
+// class Queue extends Component {
+//     constructor(props) {
+//         super(props)
+//     }
 
-    render() {
-        return (
-            <div className="App-queue">
+//     render() {
+//         return (
+//             <div className="App-queue">
         
+//             </div>
+//         )
+//     }
+
+// }
+
+const InQueue = (props) => {
+    return props.items.filter(queueItem => queueItem.status === "In Queue").map(queueItem => 
+        <div key={queueItem.id} className="InQueue-container">
+            <div className="item-title">    {queueItem.title}
             </div>
-        )
-    }
-
+            <select classname="status">
+                <option value="in-queue">
+                    In Queue
+                </option>
+                <option value="in-progress">
+                    In Progress    
+                </option>    
+                <option value="done">
+                    Done
+                </option>
+            </select>
+            <div className="created-by">
+                {queueItem.created_by}
+            </div>
+            <div className="assigned-to">
+                {queueItem.assigned_to}
+            </div>
+        </div>    
+    )
 }
-
  
-export default Queue;
+export default InQueue;
